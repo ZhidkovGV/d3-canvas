413b991327d2a187a07d23416903257ab63a249d
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-07-25 16:54:37 +0300

    some styles and components added

---
+++ src/app/components/axes/axes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-axes]',
+  templateUrl: './axes.component.html',
+  styleUrls: ['./axes.component.css']
+})
+export class AxesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('AMMA WORKING BITCH');
+  }
+
+}
