4db0a9e64cbedf47cb747d184693bfbcd898321f
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-07-31 15:19:20 +0300

    feature: performace optimiztion

--- src/app/components/canvas/canvas.component.ts
+++ src/app/components/canvas/canvas.component.ts
@@ -10,18 +10,26 @@ import {DEAD_CELL, SIZE_OF_CELL} from '../../game-config';
 export class CanvasComponent implements AfterContentInit {
   @ViewChild('canvas') canvas: ElementRef;
 
+  prevGeneration: number[][];
+
   constructor(private gridData: GridDataService) {
   }
 
   ngAfterContentInit() {
     const context = this.canvas.nativeElement.getContext('2d');
     this.gridData.initPopulation();
+    this.prevGeneration = [];
     this.gridData.render$.subscribe((grid) => {
       grid.forEach((row, xIndex) => {
         row.forEach((element, yIndex) => {
-          this.drawField(context, element, xIndex, yIndex);
+          if (this.prevGeneration.length === 0) {
+            this.drawField(context, element, xIndex, yIndex);
+          } else if ((this.prevGeneration[xIndex][yIndex] !== grid[xIndex][yIndex])) {
+            this.drawField(context, element, xIndex, yIndex);
+          }
         });
       });
+      this.prevGeneration = grid;
     });
   }
 
